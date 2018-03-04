e397cb47a7954e64846bce7c71adc7c20b4fcbd3
Ilia Okunev <iliaokunev@gmail.com>
2018-03-04 21:24:54 +0300

    additional_5

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	
+	function memberOfBracketsConfig (bracketPair, index, array) {
+		let bracket = this;
+		if(bracketPair[0] == bracket || bracketPair[1] == bracket) {
+			return bracketPair;
+		}
+	}
+	
+	let bracketsStack = new Array();
+	for(let i=0; i < str.length; i++) {
+		let currentBraket = str[i];
+		let bracketPair = bracketsConfig.find(memberOfBracketsConfig, currentBraket);
+		
+		// handle open bracket
+		if(currentBraket == bracketPair[0]) {
+			
+			/* 
+			 * Check if open close brakets has same symbole
+			 * and handle it.
+			 */
+			if(currentBraket == bracketPair[1]
+				&& bracketsStack[bracketsStack.length - 1] == bracketPair[0]) {
+				
+				bracketsStack.pop();
+				continue;					
+			}
+			bracketsStack.push(currentBraket);
+			
+		// handle close bracket
+		} else if(currentBraket == bracketPair[1]) {
+			if(bracketsStack.pop() != bracketPair[0]) {
+				return false;
+			} 
+		}
+	}
+	return bracketsStack.length == 0;
 }
